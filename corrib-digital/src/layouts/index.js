fd77f97187215a3b861551d4286b8ec23e49f64f
Michael McAndrew <michael@corribdigital.com>
2017-09-14 11:46:48 +0100

    Added SCSS

--- corrib-digital/src/layouts/index.js
+++ corrib-digital/src/layouts/index.js
@@ -3,40 +3,35 @@ import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 
-import './index.css'
+import './index.scss'
 
 const Header = () =>
   <div
     style={{
-      background: 'rebeccapurple',
       marginBottom: '1.45rem',
     }}
   >
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
         padding: '1.45rem 1.0875rem',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+      <Link
+        to="/"
+        style={{
+          color: 'black',
+          textDecoration: 'none',
+        }}
+      >
+        <img src={require('../images/logo.svg')} className="logo" />
+      </Link>
     </div>
   </div>
 
 const TemplateWrapper = ({ children }) =>
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Corrib Digital dot com"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -45,8 +40,6 @@ const TemplateWrapper = ({ children }) =>
     <Header />
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
         padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
       }}
